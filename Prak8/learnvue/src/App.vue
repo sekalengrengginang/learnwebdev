62f8cb04406702190b4e5af294cd22d1d31c8bc2
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-06-04 09:03:09 +0700

    feat: add praktikum 6-8

---
+++ Prak8/learnvue/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld />
+  <br />
+  <h2>Nama saya adalah {{ nama }}</h2>
+  <h2>Umur saya {{ umur }}</h2>
+  <h1>Bilangan {{ bilangan }}</h1>
+  <button v-on:click="Tambah">Tambah</button>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      nama: "Supri",
+      umur: 20,
+      bilangan: 0,
+    };
+  },
+  mounted() {
+    this.umur = this.umur + 30;
+  },
+  methods: {
+    Tambah() {
+      this.bilangan += 2;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ Prak8/learnvue/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="greetings">
+    <h1>Hello World from vue!</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
