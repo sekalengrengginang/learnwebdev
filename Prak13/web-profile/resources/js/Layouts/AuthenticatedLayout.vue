dbb8f9701f83dc339e03ec390ce79f9c1eb1d860
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2023-07-08 13:07:28 +0700

    fix: laravel

--- Prak13/web-profile/resources/js/Layouts/AuthenticatedLayout.vue
+++ Prak13/web-profile/resources/js/Layouts/AuthenticatedLayout.vue
@@ -29,7 +29,7 @@ const showingNavigationDropdown = ref(false);
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('post.Index')" :active="route().current('dashboard')">
+                                <NavLink :href="route('posts.index')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
                             </div>
--- Prak13/web-profile/routes/web.php
+++ Prak13/web-profile/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('posts',PostController::class);
+    Route::resource('posts', PostController::class);
 });
 
 require __DIR__.'/auth.php';
